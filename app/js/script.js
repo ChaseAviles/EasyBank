32c0d7a70029ee6cd0fe65d2883c59534e3a2a1b
Chase Aviles <chaseaviles@yahoo.com>
2020-08-24 00:04:48 -0400

    finished nav, animation added

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,34 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const overlay = document.querySelector(".overlay");
+const menu = document.querySelector(".header__menu");
+const body = document.querySelector("body");
 
 btnHamburger.addEventListener("click", function () {
-  btnHamburger.classList.add("open");
+  //close hamburger menu
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+    body.classList.remove("noScroll");
+
+    overlay.classList.remove("fade-in");
+    menu.classList.remove("fade-in");
+
+    overlay.classList.add("fade-out");
+    menu.classList.add("fade-out");
+
+    overlay.style.visibility = "hidden";
+    menu.style.visibility = "hidden";
+  } else {
+    //open hamburger menu
+    btnHamburger.classList.add("open");
+    body.classList.add("noScroll");
+
+    overlay.style.visibility = "visible";
+    menu.style.visibility = "visible";
+
+    overlay.classList.add("fade-in");
+    menu.classList.add("fade-in");
+
+    overlay.classList.remove("fade-out");
+    menu.classList.remove("fade-out");
+  }
 });
